704da120317c32399a8b55ab3d377dc39a8b826d
Parth Shah <parthshah@Parths-MacBook-Pro-3.local>
2025-02-26 15:48:54 -0500

    simpler approach

--- src/components/MetaTags.jsx
+++ src/components/MetaTags.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet';
+import { useEffect, useState } from 'react';
 
 const MetaTags = ({ 
   title, 
@@ -6,24 +7,48 @@ const MetaTags = ({
   image, 
   url = window.location.href 
 }) => {
+  const [metaData, setMetaData] = useState({
+    title: title,
+    description: description,
+    image: image,
+    url: url
+  });
+
+  useEffect(() => {
+    // Get meta data from headers if available
+    const ogTitle = document.querySelector('meta[name="x-og-title"]')?.content;
+    const ogDescription = document.querySelector('meta[name="x-og-description"]')?.content;
+    const ogImage = document.querySelector('meta[name="x-og-image"]')?.content;
+
+    if (ogTitle || ogDescription || ogImage) {
+      setMetaData({
+        title: ogTitle || title,
+        description: ogDescription || description,
+        image: ogImage || image,
+        url
+      });
+    }
+  }, [title, description, image, url]);
+
   return (
     <Helmet>
       {/* Standard meta tags */}
-      <title>{title}</title>
-      <meta name="description" content={description} />
+      <title>{metaData.title}</title>
+      <meta name="description" content={metaData.description} />
 
       {/* Open Graph meta tags */}
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
+      <meta property="og:url" content={metaData.url} />
+      <meta property="og:title" content={metaData.title} />
+      <meta property="og:description" content={metaData.description} />
+      <meta property="og:image" content={metaData.image} />
 
       {/* Twitter Card meta tags */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:url" content={metaData.url} />
+      <meta name="twitter:title" content={metaData.title} />
+      <meta name="twitter:description" content={metaData.description} />
+      <meta name="twitter:image" content={metaData.image} />
     </Helmet>
   );
 };
