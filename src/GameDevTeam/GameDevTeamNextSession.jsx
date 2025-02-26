609cf9cd4789afac973798100d356ade537fe16f
Parth Shah <parthshah@Parths-MacBook-Pro-3.local>
2025-02-26 14:43:06 -0500

    image previews for the subpages

--- src/GameDevTeam/GameDevTeamNextSession.jsx
+++ src/GameDevTeam/GameDevTeamNextSession.jsx
@@ -7,8 +7,7 @@ const GameDevTeamNextSession = () => {
       <h2>When's the next session?</h2>
       <div className="session-cards">
         <div className="session-card">
-          {/* <p className="session-date">June 9–July 18</p> */}
-          <h3>July 9 - July 18</h3>
+          <h3>June 9 - July 18</h3>
           <p className="session-time">Tuesdays and Thursdays, 6–8 PM PST</p>
         </div>
       </div>
