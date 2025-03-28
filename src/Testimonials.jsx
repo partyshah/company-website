a88a93b057d23a4bddd7863a798e399080003181
mikeschahfer <mikeschahfer@gmail.com>
2025-03-28 15:50:31 -0500

    Suchitra video, infosession bar on every page, made the boxes on team pages smaller, and changed an FAQ

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -3,7 +3,7 @@ import './Testimonials.css';
 import testimonial1Video from './images/testimonial1.mp4';
 import testimonial2Video from './images/testimonial2.mp4';
 import testimonial3Video from './images/testimonial3.mp4';
-import Suchitra from './images/Suchitra.mp4';
+import SuchitraVideo from './images/Suchitra_testimonial.mp4';
 import danielleProfile from './images/danielle-pitts.jpeg';
 import scottProfile from './images/scott-photo.jpeg';
 import bryceProfile from './images/Bryce.png';
@@ -23,7 +23,7 @@ const Testimonials = () => {
     {
       type: 'video',
       videoId: 'suchitra',
-      videoSrc: Suchitra,
+      videoSrc: SuchitraVideo,
       backgroundColor: "#44E4A7"  // Green
     },
     {
