6fbc1d93db637b0923bb7673fe276a4509194a92
Tumhara Naam <tumhara@email.com>
2025-05-08 15:40:14 +0530

    contant update

--- src/GameDevTeam/GameDevTeamHeader.jsx
+++ src/GameDevTeam/GameDevTeamHeader.jsx
@@ -8,7 +8,7 @@ const GameDevTeamHeader = () => {
       <div className="header-content">
         <div className="level-badge intro">Advanced</div>
         <h1>Game Development Program</h1>
-		<h2 className="level-badge-sub-content>Build Games. Build Skills. Build Your Future</h2>
+		<h2 className="level-badge-sub-content">Build Games. Build Skills. Build Your Future</h2>
         <p>Join a 15 person team of experienced high school computer science students, get coached by an expert game developer, and build a multiplayer game to compete in our end-of-season showcase. Our game development program gives you the skills, experience, and community to help you grow as a developer and creator.</p>
         <div className="outcome-section">
           <h2>Outcome</h2>
